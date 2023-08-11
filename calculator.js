f712ddcb90c2d5a793fb25e3874454e3ecb15c83
pradeep <91619828+adee140299@users.noreply.github.com>
2023-08-11 11:09:13 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,84 @@
+function fun1(){
+let inputtext=document.getElementById("input");
+inputtext.value +=document.getElementById("btn1").value;
+}
+function fun2(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn2").value;
+    }
+      function fun3(){
+        let inputtext=document.getElementById("input");
+        inputtext.value +=document.getElementById("btn3").value;
+        }
+        function fun4(){
+            let inputtext=document.getElementById("input");
+            inputtext.value +=document.getElementById("btn4").value;
+            }
+function fun5(){
+     let inputtext=document.getElementById("input");
+     inputtext.value +=document.getElementById("btn5").value;
+}
+function fun6(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn6").value;
+}
+function fun7(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn7").value;
+}
+function fun8(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn8").value;
+}
+function fun9(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn9").value;
+}
+function fun10(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn10").value;
+}
+function fun11(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn11").value;
+}
+function fun12(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn12").value;
+}
+function fun13(){
+    // let inputtext=document.getElementById("input");
+    // inputtext.value +=document.getElementById("btn13").value;
+    document.getElementById("input").value="";
+}
+function fun14(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn14").value;
+}
+function fun15(){
+    
+    document.getElementById("input").value=eval(document.getElementById("input").value)
+
+}
+function fun16(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn16").value;
+}
+function fun17(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn17").value;
+}
+function fun18(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn18").value;
+}
+function fun19(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn19").value;
+}
+function fun20(){
+    let inputtext=document.getElementById("input");
+    inputtext.value +=document.getElementById("btn20").value;
+}
+
+
